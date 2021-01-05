978cdd7cfe27620f72b24b0fdc4000d955f1fb0e
bhuschle <72930166+bhuschle@users.noreply.github.com>
2021-01-05 17:48:53 -0500

    origin commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = 'da3a67ae-e504-441f-b7e2-d2bc8cb98b6e';
+export const MODAL_CLOSE_SEC = 2.5;
